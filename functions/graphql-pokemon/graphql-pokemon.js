25bb334f68bd881db185c6d3d709b706dd0a2141
Bryan Kegley <hi@bryankegley.me>
2019-07-23 16:40:15 -0500

    fix pokemon

--- functions/graphql-pokemon/graphql-pokemon.js
+++ functions/graphql-pokemon/graphql-pokemon.js
@@ -3,15 +3,11 @@ const {ApolloServer} = require('apollo-server-lambda')
 
 const schema = require('./schema.graphql')
 const resolvers = require('./resolvers')
-const data = require('../data.json')
 
 exports.handler = async function(event, context) {
   const server = new ApolloServer({
     typeDefs: schema,
     resolvers,
-    context: () => ({
-      data,
-    }),
     playground: true,
     introspection: true,
   })
